804a4a7f6d129e4de1236bd8c49fe22cd41ae921
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-10-19 13:58:18 -0200

    formatting and using PHPUnit 4

--- DependencyInjection/EjsmontCircuitBreakerExtension.php
+++ DependencyInjection/EjsmontCircuitBreakerExtension.php
@@ -7,14 +7,17 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
-class EjsmontCircuitBreakerExtension extends Extension {
+class EjsmontCircuitBreakerExtension extends Extension
+{
 
-    public function load(array $configs, ContainerBuilder $container) {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
 
-    public function getAlias() {
+    public function getAlias()
+    {
         return 'ejsmont_circuit_breaker';
     }
 
--- Storage/DoctrineCacheAdapter.php
+++ Storage/DoctrineCacheAdapter.php
@@ -3,28 +3,31 @@
 namespace Ejsmont\CircuitBreakerBundle\Storage;
 
 use Ejsmont\CircuitBreaker\Storage\Adapter\BaseAdapter;
+use Ejsmont\CircuitBreaker\Storage\StorageException;
 use Doctrine\Common\Cache\Cache;
 
 /**
  * Class is an adapter between Doctrine\Cache and Ejsmont\CircuitBreaker\Storage
- * 
+ *
  * I use instance of cahe provided by your symfony application (doctrine cache)
  */
-class DoctrineCacheAdapter extends BaseAdapter {
+class DoctrineCacheAdapter extends BaseAdapter
+{
 
     /**
      * Instance to be used by the circuit breaker
-     * @var \Doctrine\Common\Cache\Cache $doctrineCacheInstance 
+     * @var \Doctrine\Common\Cache\Cache $doctrineCacheInstance
      */
     private $doctrineCacheInstance;
 
     /**
-     * Expects doctring cache implementation instance to route 
+     * Expects doctring cache implementation instance to route
      * circuit breaker calls. Instance have to be ready to use.
-     * 
+     *
      * @param \Doctrine\Common\Cache\Cache $doctrineCacheInstance
      */
-    public function __construct(Cache $doctrineCacheInstance) {
+    public function __construct(Cache $doctrineCacheInstance)
+    {
         $this->doctrineCacheInstance = $doctrineCacheInstance;
     }
 
@@ -32,33 +35,36 @@ class DoctrineCacheAdapter extends BaseAdapter {
      * If you provided instance of doctrine cache we assume that it is ready to go.
      * @return boolean
      */
-    protected function checkExtension() {
+    protected function checkExtension()
+    {
         return true;
     }
 
     /**
      * Loads item by cache key.
-     * 
+     *
      * @param string $key
      * @return mixed
-     * 
-     * @throws Ejsmont\CircuitBreaker\Storage\StorageException if storage error occurs, handler can not be used
+     *
+     * @throws StorageException if storage error occurs, handler can not be used
      */
-    protected function load($key) {
+    protected function load($key)
+    {
         return $this->doctrineCacheInstance->fetch($key);
     }
 
     /**
      * Save item in the cache.
-     * 
+     *
      * @param string $key
      * @param string $value
      * @param int $ttl
      * @return void
-     * 
-     * @throws Ejsmont\CircuitBreaker\Storage\StorageException if storage error occurs, handler can not be used
+     *
+     * @throws StorageException if storage error occurs, handler can not be used
      */
-    protected function save($key, $value, $ttl) {
+    protected function save($key, $value, $ttl)
+    {
         $this->doctrineCacheInstance->save($key, $value, $ttl);
     }
 
